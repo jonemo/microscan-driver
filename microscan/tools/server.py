433a4b519aba7726d9186fc9f52d5e2faa472c2a
Jonas Neubert <jonas@tempoautomation.com>
2017-05-17 00:07:57 -0700

    finish up “MVP” of xmlrpc server command line tool

--- microscan/tools/server.py
+++ microscan/tools/server.py
@@ -1,25 +1,33 @@
 from argparse import ArgumentParser
 from sys import exit
+from xmlrpc.server import SimpleXMLRPCServer
 
 from microscan.driver import MS3Driver
 
 
 parser = parser = ArgumentParser(
-    description='XMLRPC server interface to Microscan barcode scanner')
+    description='XMLRPC server interface to Microscan barcode reader')
 parser.add_argument(
-    'port', type=str, help='an integer for the accumulator')
-args = parser.parse_args()
+    'port', type=int, help='Port number for XMLRPC server')
+parser.add_argument(
+    'device', type=str,
+    help='Serial port device name where reader is connected')
 
 
 def main():
-    driver = MS3Driver(port=args.port)
-    driver.connect()
+    args = parser.parse_args()
+
+    server = SimpleXMLRPCServer(("localhost", args.port))
+
     try:
-        while True:
-            print(driver.port.readline())
-    except:
-        driver.close()
-        return 0
+        with MS3Driver(args.device) as driver:
+            server.register_instance(driver, allow_dotted_names=True)
+            server.register_introspection_functions()
+            server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    return 0
 
 
 if __name__ == '__main__':
