65d5bb8b39be1c526c10a8ecd5a65ccf27b965c9
Jonas Neubert <jn@jonasneubert.com>
2018-05-07 00:32:21 -0700

    version 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='microscan',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     install_requires=['pyserial'],
